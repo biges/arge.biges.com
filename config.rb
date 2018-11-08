c38cffc6c4e18c7e8ca5602ce453ab2a2d4ed10b
Uğur Özyılmazel <ugur.ozyilmazel@biges.com>
2018-11-08 21:54:39 +0300

    Career page

--- config.rb
+++ config.rb
@@ -71,6 +71,7 @@ activate :directory_indexes
 proxy "/arsiv/index.html", "/pages/archive.html"
 proxy "/hakkinda/index.html", "/pages/about.html"
 proxy "/yazilim-ekibi/index.html", "/pages/team.html"
+proxy "/kariyer/index.html", "/pages/career.html"
 
 activate :deploy do |deploy|
   deploy.build_before = true
