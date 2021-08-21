7f94153bbc6ae7e12e2d21d5abee09c773dc7ed1
Uğur Özyılmazel <ugur.ozyilmazel@biges.com>
2021-08-21 14:07:34 +0300

    update gems

--- config.rb
+++ config.rb
@@ -75,7 +75,7 @@ proxy "/kariyer/index.html", "/pages/career.html"
 
 activate :deploy do |deploy|
   deploy.build_before = true
-  deploy.method   = :git
+  deploy.deploy_method   = :git
   deploy.remote = "origin"
   deploy.branch = "gh-pages"
 end
